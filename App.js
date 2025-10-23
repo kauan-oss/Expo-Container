bce5ac48decf022b0edcd7af611d7835eb04ef5f
Kauan <kauan.stipp.pedroso@escola.pr.gov.br>
2025-10-23 14:10:36 +0000

    a

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import Titulo from './componentes/Titulo';
+import Saudacao from './componentes/Saudacao';
+
+export default function App(){
+  return(
+    <ScrollView style={{padding:20}}>
+      <Titulo/>
+      <Saudacao nome="Kauan"/>
+    </ScrollView>
+  )
+}
