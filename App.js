4d5529dfd2541059dd42ede12f62b3611e789339
Kauan <kauan.stipp.pedroso@escola.pr.gov.br>
2025-11-06 13:03:22 +0000

    a

--- App.js
+++ App.js
@@ -2,12 +2,44 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 import Titulo from './componentes/Titulo';
 import Saudacao from './componentes/Saudacao';
+import FotoPerfil from './componentes/FotoPerfil';
+import CartaoUsuario from './componentes/CartaoUsuario';
 
 export default function App(){
-  return(
-    <ScrollView style={{padding:20}}>
-      <Titulo/>
-      <Saudacao nome="Kauan"/>
-    </ScrollView>
-  )
-}
+  return<ScrollView>
+      <Titulo />
+      <Saudacao nome='Kauan' />
+      <FotoPerfil />
+
+      <CartaoUsuario 
+      nome='João da Silva'
+      descricao = 'Programador Mobile'
+      img='perfil00'
+      />
+
+      <CartaoUsuario 
+      nome='Maria da Silva'
+      descricao = 'Médica'
+      img='perfil01'
+      />
+
+      <CartaoUsuario 
+      nome='Pamela Raiane'
+      descricao = 'Programadora Mobile'
+      img='perfil02'
+      />
+
+      <CartaoUsuario 
+      nome='Kauan Stipp'
+      descricao = 'Assistente de TI'
+      img='perfil03'
+      />
+
+      <CartaoUsuario 
+      nome='Marlon Ribeiro'
+      descricao = 'Advogado'
+      img='perfil04'
+      />
+
+      </ScrollView>
+    }
---
+++ img/fotos.js
@@ -0,0 +1,7 @@
+export default{
+    perfil00: require('./1.jpeg'),
+    perfil01: require('./2.jpeg'),
+    perfil02: require('./3.png'),
+    perfil03: require('./4.png'),
+    perfil04: require('./5.jpg'),
+}
